07a35d19c162424eb6f4d70a103f6d9ed1bafd19
Mudit Garg <anilmudit@gmail.com>
2020-05-03 20:18:46 -0400

    refactor

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ data = readPoints('points3D.txt')';
 custom_box = [1 1 0; 1 -1 0; -1 -1 0; -1 1 0; 1 1 2; 1 -1 2; -1 -1 2; -1 1 2];
 
 % Ransac routine
-[p_best, n_best, ro_best, X_best, Y_best, Z_best, error_best] = ransac_cus(data, 400, 10, 2, 40);
+[p_best, n_best, ro_best, error_best] = ransac_cus(data, 400, 10, 2, 40);
 
 % Get rotation matrix and translation created box
 rot_mat = generate_rotation(n_best(1), n_best(2), n_best(3));
