cb23d7717d2801477114ce37efb48e0fc46e0a6b
Mudit Garg <anilmudit@gmail.com>
2020-05-03 20:13:20 -0400

    refactor

--- least_square_error.m
+++ least_square_error.m
@@ -1,7 +1,4 @@
-function [n_est ro_est X Y Z]=least_square_error(p)
-%© Tim Zaman 2010, input: p (points)
-% Works like [n_est ro_est X Y Z]=LSE(p)
-% p should be a Mx3; [points x [X Y Z]]
+function [n_est ro_est ]=least_square_error(p)
 
 pbar=mean(p);
 
@@ -11,7 +8,7 @@ end
 
 %Sum up all entries in A
 Asum=sum(A,3);
-[V ~]=eig(Asum);
+[V, ~]=eig(Asum);
 
 %Calculate new normal vector
 n_est=V(:,1);
@@ -19,6 +16,4 @@ n_est=V(:,1);
 %Calculate new ro
 ro_est=dot(n_est,pbar);
 
-[X,Y]=meshgrid(min(p(:,1)):max(p(:,1)),min(p(:,2)):max(p(:,2)));
-Z=(ro_est-n_est(1)*X-n_est(2).*Y)/n_est(3);
 end
